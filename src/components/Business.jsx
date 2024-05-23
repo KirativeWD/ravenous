85e126ebf429823b3f238f12d13dd36913a8e2f2
TantariWD <kira.businger@outlook.com>
2024-05-23 12:53:12 -0400

    Created Business component with sample business info

---
+++ src/components/Business.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Business() {
+    return (
+        <li>
+            <article>
+                <img src="" alt="" />
+                <div>
+                    <h3>Restaurant Name</h3>
+                    <div>
+                        <p>Food Category</p>
+                        <span>Stars</span>
+                        <span>Reviews</span>
+                    </div>
+                    <p>123 Fake St, City, State Zip</p>
+                </div>
+            </article>
+        </li>
+    )
+}
+
+export default Business;
