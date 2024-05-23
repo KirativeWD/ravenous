33efd15a986e6c9aaca581f2ff2538490f3a01e5
TantariWD <kira.businger@outlook.com>
2024-05-23 15:27:27 -0400

    Created generic business data for pt 1

---
+++ src/data/businessData.js
@@ -0,0 +1,52 @@
+// Business data used for Ravenous Part 1
+
+const businessData = [
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&1',
+        name: 'Generic Name',
+        category: 'Italian',
+        stars: 4,
+        reviews: 134,
+        address: '123 Street, City, State Zip'
+    },
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&2',
+        name: 'Generic Name',
+        category: 'Greek',
+        stars: 5,
+        reviews: 256,
+        address: '123 Street, City, State Zip'
+    },
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&3',
+        name: 'Generic Name',
+        category: 'Mexican',
+        stars: 4.5,
+        reviews: 148,
+        address: '123 Street, City, State Zip'
+    },
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&4',
+        name: 'Generic Name',
+        category: 'Italian',
+        stars: 5,
+        reviews: 302,
+        address: '123 Street, City, State Zip'
+    },
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&5',
+        name: 'Generic Name',
+        category: 'Greek',
+        stars: 4.5,
+        reviews: '198',
+        address: '123 Street, City, State Zip'
+    },
+    {
+        img: 'https://source.unsplash.com/random/?Restaurant&6',
+        name: 'Generic Name',
+        category: 'French',
+        stars: 3,
+        reviews: 200,
+        address: '123 Street, City, State Zip'
+    },
+];
